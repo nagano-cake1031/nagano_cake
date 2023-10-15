facb10fae32fa0c387ce61f772755508babe484d
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-10-15 13:35:12 +0000

    itemsのモデル作

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 2023_10_15_095932) do
 
   create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
