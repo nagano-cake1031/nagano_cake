63ef99948cfde83860b849dfc2b942a662b3640e
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-10-24 12:57:13 +0000

    20231024

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+
+  before_action :customer_state, only: [:create]
+
   def after_sign_in_path_for(resource)
-    public_path
+    public_items_path
   end
 
   def after_sign_out_path_for(resource)
     new_customer_session_path
   end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -25,12 +29,22 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  protected
+
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.is_active
+      redirect_to new_customer_registration_path
+    end
+  end
+
   private
 
   def session_params
