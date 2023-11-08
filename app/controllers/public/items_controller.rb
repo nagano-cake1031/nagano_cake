8c3aed2ae09f068a90dd052c0073d18e97ccaed2
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-11-08 16:08:20 +0000

    231109_0108

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,6 +8,13 @@ class Public::ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def create
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id
+    @item.save
+    redirect_to cart_items_path
+  end
+
   private
 
   def item_params
