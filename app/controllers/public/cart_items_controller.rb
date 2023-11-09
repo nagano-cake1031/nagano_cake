604f6340d049d113e074d9e21609bf6c8fca0730
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-11-09 13:34:13 +0000

    231109_2234

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -20,8 +20,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destry
-    cart_item = cart_item.find(params[:id])
-    cart_item.destroy
+    @cart_item = cart_item.find(params[:id])
+    @cart_item.customer_id = current_customer.id
+    @cart_item.destroy
     redirect_to cart_items_path
   end
 
