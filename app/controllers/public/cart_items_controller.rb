8e434e2a7bc0973c4aa69d37012d651f1fc56ca1
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-11-09 06:27:54 +0000

    231109_1527

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,7 +6,7 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.user_id = current_user.id
+    @cart_item.customer_id = current_customer.id
     @cart_item.save
     redirect_to cart_items_path
   end
