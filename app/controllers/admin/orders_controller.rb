a8384c6dd5da3d357ea080915a17394addb31079
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-12-02 06:03:47 +0000

    231202_1503

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,13 @@
 class Admin::OrdersController < ApplicationController
+
+ def show
+   @order = Order.find(params[:id])
+ end
+
+ private
+
+def order_params
+ params.require(:order).permit(:payment_method, :customer_id, :total_price, :postage, :address, :post_code, :name)
+end
+
 end
