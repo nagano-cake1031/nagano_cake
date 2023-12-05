2852c8f27c9eb8b0165b830015df7c5e5d607c84
hinakokko <135841488 +hanakokko@users.noreply.github.com>
2023-12-05 14:37:19 +0000

    231205_2337

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -46,8 +46,7 @@ end
 def show
  @order = Order.find(params[:id])
  @sum = 0
- @order_detail = OrderDetail.find(params[:id])
- @order_details = OrderDetail.all
+ @order_details = OrderDetail.where(order_id: params[:id])
 end
 
 private
